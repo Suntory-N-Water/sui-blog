32bd1ac4359ae87acf46e9cf8005a97ee49c9a84
sui <1202ayasnppk@gmail.com>
2026-09-22 15:25:35 +0900

    feat: 脚注から本文へ戻るリンクを追加する

--- src/components/portable-text/components.ts
+++ src/components/portable-text/components.ts
@@ -2,9 +2,13 @@ import ArticleImage from './ArticleImage.astro';
 import Block from './Block.astro';
 import BlockquoteGroup from './BlockquoteGroup.astro';
 import CodeBlock from './CodeBlock.astro';
+import FootnoteLink from './FootnoteLink.astro';
 
 export const articlePortableTextComponents = {
   block: Block,
+  mark: {
+    link: FootnoteLink,
+  },
   type: {
     blockquoteGroup: BlockquoteGroup,
     code: CodeBlock,
