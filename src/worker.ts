1ec5801071736ca6ed929f75b61154a6a339d48d
sui <1202ayasnppk@gmail.com>
2026-09-23 09:26:11 +0900

    Revert "chore: 記事ページの処理ごとの所要時間をログに出す"

--- src/worker.ts
+++ src/worker.ts
@@ -5,27 +5,7 @@ import handler, {
 
 export { PluginBridge };
 
-const isolateStartedAt = Date.now();
-let requestCount = 0;
-
 export default {
   ...handler,
-  async fetch(request, env, ctx) {
-    const start = performance.now();
-    requestCount += 1;
-    const response = await handler.fetch(request, env, ctx);
-    const { pathname } = new URL(request.url);
-    if (pathname.startsWith('/blog/')) {
-      console.log(
-        `[worker-timing] ${JSON.stringify({
-          pathname,
-          headersMs: Math.round(performance.now() - start),
-          isolateAgeMs: Date.now() - isolateStartedAt,
-          requestCount,
-        })}`,
-      );
-    }
-    return response;
-  },
   scheduled: createScheduledHandler(),
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler;
